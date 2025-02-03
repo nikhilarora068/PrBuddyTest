a570b8b071cc61b1e6780a71446ceeb2cc0d159b
Nikhil Arora <nikhilarora068@gmail.com>
2025-02-03 20:30:10 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n: int) -> int:
+            if n <= 0:
+                return 0
+            elif n == 1:
+                return 0
+            elif n == 2:
+                return 1
+            else:
+                return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
